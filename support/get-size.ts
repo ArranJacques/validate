da3dd238d7c2396bb180513401155ee32c43cab8
Arran Jacques <arranjacques@gmail.com>
2021-01-17 11:52:39 +0000

    Fix get size function when returning value for number

--- support/get-size.ts
+++ support/get-size.ts
@@ -3,7 +3,7 @@ export default function getSize(value: any): number | null {
     if (typeof value === 'string') {
         return value.length;
     } else if (typeof value === 'number') {
-        return value.toString().length;
+        return value;
     } else if (Array.isArray(value)) {
         return value.length;
     }
